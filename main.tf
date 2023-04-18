f22842fa3f59641d88d96be10d41207e3cf2d0b8
David Landers <david.a.landers@outlook.com>
2023-04-18 15:29:25 -0600

    Updated main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,6 @@ resource "azurerm_subnet" "mtc-subnet" {
   name                 = "mtc-subnet"
   resource_group_name  = azurerm_resource_group.mtc-rg.name
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-  address_prefixes     = ["10.123.5.0/24"]
+  address_prefixes     = ["10.123.1.0/24"]
 }
 
